6fcc107e4ac117a44883d962c0ba1dd338c04b3b
udeesha <udeesha@mi-synergy.com>
2019-08-22 16:26:42 +0530

    Added repository configaration

--- src/main/java/com/lk/ijse/main/WebAppConfig.java
+++ src/main/java/com/lk/ijse/main/WebAppConfig.java
@@ -1,7 +1,9 @@
 package com.lk.ijse.main;
 
+import com.lk.ijse.repository.GemRepository;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -10,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @ComponentScan({"com.lk.ijse"})
 @EnableWebMvc
+@EnableJpaRepositories(basePackageClasses = GemRepository.class)
 @EnableTransactionManagement
 public class WebAppConfig implements WebMvcConfigurer {
 
