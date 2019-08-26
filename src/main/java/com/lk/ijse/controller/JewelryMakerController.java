bacaed32ce394e34ffad48e965b75018ccfe0d2d
Udeesha Induwara <udeesha@mi-synergy.com>
2019-08-26 10:54:17 +0530

    Added save getAll delete methods to jewelryMakerService.

--- src/main/java/com/lk/ijse/controller/JewelryMakerController.java
+++ src/main/java/com/lk/ijse/controller/JewelryMakerController.java
@@ -1,10 +1,12 @@
 package com.lk.ijse.controller;
 
+import com.lk.ijse.dto.JewelryMakerDto;
 import com.lk.ijse.service.JewelryMakerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @CrossOrigin
@@ -13,4 +15,19 @@ public class JewelryMakerController {
 
     @Autowired
     private JewelryMakerService jewelryMakerService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public boolean saveJewelryMaker(@RequestBody JewelryMakerDto jewelryMaker) {
+        return jewelryMakerService.saveJewelryMaker(jewelryMaker);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ArrayList<JewelryMakerDto> getAllJewelryMaker() {
+        return jewelryMakerService.getAllJewelryMaker();
+    }
+
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public boolean deleteJewelryMaker(@PathVariable("id") int jewelryMakerId) {
+        return jewelryMakerService.deleteJewelryMaker(jewelryMakerId);
+    }
 }
